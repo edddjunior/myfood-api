fbc09cdf3453c2c3b0fea05c1902e38a1bf4f7e3
edddjunior <edsonbergamojunior@gmail.com>
2019-04-29 16:01:43 -0300

    Method set_price added | Nested attributes permitted

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
+  before_validation :set_price
+
+  accepts_nested_attributes_for :order_products, allow_destroy: true
+  # When we create an order, we'll accept the attributes from order_products too
+
   belongs_to :restaurant
 
   has_many :order_products
@@ -8,4 +13,14 @@ class Order < ApplicationRecord
   validates :total_value, presence: true
 
   enum status: { waiting: 0, delivered: 1 }
+
+  private
+    def set_price
+      @final_price = 0
+      order_products.each do |order_product|
+        product = Product.find order_product.product_id
+        @final_price += order_product.quantity * product.price
+      end
+      self.total_value = @final_price
+    end
 end
